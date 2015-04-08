04d2ada05ddfb0cd1c8aaad65b30c0c63ecf1705
Joey Buczek <joe.buczek@gmail.com>
2015-04-08 02:13:46 +0000

    event controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get '/about' => 'welcome#about'
   
+  namespace :api, default: { format: :json } do
+    resources :events, only: [:create]
+  end
+  
   resources :applications
 
   devise_for :users
