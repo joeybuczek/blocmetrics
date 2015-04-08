2bd1d0ce5417a839341f320e190874f1ab3afe1b
Joey Buczek <joe.buczek@gmail.com>
2015-04-08 03:43:37 +0000

    updated events create

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -4,13 +4,13 @@ class API::EventsController < ApplicationController
   def create
     @application = Application.find_by(url: request.env['HTTP_ORIGIN'])
     if @application.nil?
-      render json: "Unregistered application: #{params}", status: :unprocessable_entity
+      render json: "Unregistered application: #{event_params}", status: :unprocessable_entity
     else
-      @event = @application.events.build(event_params)
+      @event = @application.events.build(name: event_params)
       if @event.save
-        # render json: @event, status: :created
+        render json: @event, status: :created
       else
-        # render @event.errors, status: :unprocessable_entity
+        render @event.errors, status: :unprocessable_entity
       end
     end
   end
@@ -18,9 +18,13 @@ class API::EventsController < ApplicationController
   private
   
   def event_params
-    params.require(:event).permit(:name)
+    params.require(:name)
   end
   
-  # curl -v -H "Accept: application/json" -H "Origin: http://test.app.com" "Content-type: application/json" -X POST -d '{"name":"foobar"}' http://localhost:3000/api/events
+  # Curl testing payload:
+  # curl -v -H "Accept: application/json" -H "Origin: http://test.app.com" "Content-type: application/json" -X POST -d 'name=foobar' http://localhost:3000/api/events
+  
+  # The params received from the above will look like this:
+  # {"name"=>"foobar", "action"=>"create", "default"=>{"format"=>:json}, "controller"=>"api/events"}
   
 end
