12877c00daa63a032129dc3c2660243ffc1093e7
Joey Buczek <joe.buczek@gmail.com>
2015-04-17 03:22:33 +0000

    event testing 2

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,20 +2,20 @@ class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def create
-    @application = Application.last
-    @event = @application.events.build(name: "bypass")
-    @event.save
-#     @application = Application.find_by(url: request.env['HTTP_ORIGIN'])
-#     if @application.nil?
-#       render json: "Unregistered application: #{event_params}", status: :unprocessable_entity
-#     else
-#       @event = @application.events.build(name: event_params)
-#       if @event.save
-#         render json: @event, status: :created
-#       else
-#         render @event.errors, status: :unprocessable_entity
-#       end
-#     end
+#     @application = Application.last
+#     @event = @application.events.build(name: "bypass")
+#     @event.save
+    @application = Application.find_by(url: request.env['HTTP_ORIGIN'])
+    if @application.nil?
+      render json: "Unregistered application: #{event_params}", status: :unprocessable_entity
+    else
+      @event = @application.events.build(name: event_params)
+      if @event.save
+        render json: @event, status: :created
+      else
+        render @event.errors, status: :unprocessable_entity
+      end
+    end
   end
   
   private
@@ -25,7 +25,7 @@ class API::EventsController < ApplicationController
   end
   
   # Curl testing payload:
-  # curl -v -H "Accept: application/json" -H "Origin: http://test.app.com" "Content-type: application/json" -X POST -d 'name=foobar' http://localhost:3000/api/events
+  # curl -v -H "Accept: application/json" -H "Origin: http://test.app.com" "Content-type: application/json" -X POST -d 'name=foobar' https://joey-blocmetrics.herokuapp.com/api/events
   
   # The params received from the above will look like this:
   # {"name"=>"foobar", "action"=>"create", "default"=>{"format"=>:json}, "controller"=>"api/events"}
