11006b568e4275b119d9894175014067b53923a0
Joey Buczek <joe.buczek@gmail.com>
2015-04-17 06:45:03 +0000

    Updated dependency on models

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,6 @@
 class Application < ActiveRecord::Base
   belongs_to :user
-  has_many :events
+  has_many :events, dependent: :destroy
   
   validates :name, presence: true
   validates :url, presence: true
